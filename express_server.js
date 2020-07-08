f46dbc8e98974d2ef37218ae490510e7cd6fd5f2
Sara Irving <sarakirving@gmail.com>
2020-07-08 04:59:02 +0000

    Created skeleton for users object

--- express_server.js
+++ express_server.js
@@ -17,6 +17,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
+
 const randomOptions = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const randomLength = 6;
 function generateRandomString(length, content) {
