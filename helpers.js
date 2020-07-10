a137e4cf981a5ebad2320aea5c8f25323b4b7a8d
Sara Irving <sarakirving@gmail.com>
2020-07-10 01:36:43 +0000

    Refactored function, removed the return null statement. Now it will either return the email or return undefined by default

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 const getUserByEmail = function(email, database) {
+  console.log('email, database = ', email, database);
   for (let element in database) {
     if (database[element].email === email) {
       return element;
     } 
   }
-  return null;
 };
 
 
