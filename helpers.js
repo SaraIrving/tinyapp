a10fee6cafc676a126500dc4b640022188edfa11
Sara Irving <sarakirving@gmail.com>
2020-07-11 23:20:38 +0000

    Added comments describing the functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+//extracts a user's unique ID from the urlDatabase based off of their email
 const getUserByEmail = function(email, database) {
   for (let element in database) {
     if (database[element].email === email) {
@@ -6,7 +7,7 @@ const getUserByEmail = function(email, database) {
   }
 };
 
-
+//generates a random 6 character string from the options provided in randomOptions
 const randomOptions = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const randomLength = 6;
 const generateRandomString = function(length, content) {
@@ -17,15 +18,16 @@ const generateRandomString = function(length, content) {
   return randomString;
 };
 
-
+//checks that a given shortURL exists in the urlDatabase
 const checkShortURLExists = function (shortURL, database) {
   if (database[shortURL]) {
     return true;
   } else {
     return false;
   }
-}
+};
 
+//checks that the ID of the logged in user matches the ID of the shortURL in question 
 const checkIdMatches = function (userId, shortURL, database) {
   const idInDatabase = database[shortURL].userID;
   if (userId === idInDatabase) {
@@ -33,7 +35,7 @@ const checkIdMatches = function (userId, shortURL, database) {
   } else {
     return false;
   }
-}
+};
 
 
 module.exports = {getUserByEmail, 
